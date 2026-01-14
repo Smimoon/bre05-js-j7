66c344a2db4e49ade746af3efe26460958ddec36
smimoon <simonlaroche@orange.fr>
2026-01-14 16:30:04 +0000

    Save end exercice-2

--- fetch/exercice-2/index.js
+++ fetch/exercice-2/index.js
@@ -1,1 +1,8 @@
 let url = "https://maridoucet.sites.3wa.io/user-api/users";
+
+window.addEventListener("DOMContentLoaded", () => {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+});

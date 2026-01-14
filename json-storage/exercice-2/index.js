acb9216673e6fc5256f77306d9daba0554993e80
smimoon <simonlaroche@orange.fr>
2026-01-14 11:19:37 +0000

    Save end exercice-2

--- json-storage/exercice-2/index.js
+++ json-storage/exercice-2/index.js
@@ -1,12 +1,14 @@
-window.addEventListener("DOMContentLoaded", function(){
-    let username = window.prompt("Quel est votre nom ?");
-    
-    
-    sessionStorage.setItem("username", username);
-    
+window.addEventListener("DOMContentLoaded", function() {
+   
+
     let name = sessionStorage.getItem("username");
-    
-    if(name !== "") {
-        window.alert(`Bonjour ${username}`);
+
+    if (name) {
+    let username = window.prompt("Quel est votre nom ?");
+        sessionStorage.setItem("username", username);
+
+    }
+    else {
+        window.alert(`Bonjour ${name}`);
     }
 });

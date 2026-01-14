74e6bcadefd959b32e6bd8c846baa4dc5f4475f5
smimoon <simonlaroche@orange.fr>
2026-01-14 16:17:29 +0000

    Save end exercice-1

--- fetch/exercice-1/index.js
+++ fetch/exercice-1/index.js
@@ -1,1 +1,14 @@
-let url = "https://maridoucet.sites.3wa.io/user-api/user/15"
+let url = "https://maridoucet.sites.3wa.io/user-api/user/15";
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+
+window.addEventListener("DOMContentLoaded", () => {
+   fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err)); 
+});

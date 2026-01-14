486f1a26a4e20554cb4ec9f79c25ccf094a52b14
smimoon <simonlaroche@orange.fr>
2026-01-14 14:19:36 +0000

    Save end exercice-4

--- json-storage/exercice-4/index.js
+++ json-storage/exercice-4/index.js
@@ -1,9 +1,18 @@
 let user = {
-    firstName : "Harry",
-    lastName : "Bow",
-    motto : "C'est beau la vie !"
+    firstName: "Harry",
+    lastName: "Bow",
+    motto: "C'est beau la vie !"
 };
 
-document.addEventListener("DOMContentLoaded", function(){
-
+document.addEventListener("DOMContentLoaded", function() {
+    let userStorage = sessionStorage.getItem("user");
+    if (!userStorage) {
+        let objet = JSON.stringify(user);
+        sessionStorage.setItem("user", objet);
+    }
+    else {
+        let jsonUser = sessionStorage.getItem("user");
+        let newUser = JSON.parse(jsonUser);
+        console.log(newUser);
+    }
 });

483f90cf95f10ff28d2cf84dee055a7e543777b6
smimoon <simonlaroche@orange.fr>
2026-01-14 09:59:08 +0000

    Save morning break, work in progress

--- json-storage/exercice-2/index.js
+++ json-storage/exercice-2/index.js
@@ -1,3 +1,12 @@
 window.addEventListener("DOMContentLoaded", function(){
-
+    let username = window.prompt("Quel est votre nom ?");
+    
+    
+    sessionStorage.setItem("username", username);
+    
+    let name = sessionStorage.getItem("username");
+    
+    if(name !== "") {
+        window.alert(`Bonjour ${username}`);
+    }
 });

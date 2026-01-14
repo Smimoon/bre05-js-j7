6719a6f822f5fb78ba501e830790b66b42d367db
smimoon <simonlaroche@orange.fr>
2026-01-14 12:36:15 +0000

    Save start exercice-3

---
+++ json-storage/exercice-3/index.js
@@ -0,0 +1,11 @@
+let data = {
+    firstName : "Harry",
+    lastName : "Bow",
+    motto : "C'est beau la vie !"
+};
+
+let str = "{\"firstName\":\"Colonel\",\"lastName\":\"Sanders\",\"motto\":\"Il est bon mon poulet\"}";
+
+document.addEventListener("DOMContentLoaded", function(){
+
+});
